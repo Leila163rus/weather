41ce5d66d5be0f260db4cde55fee3869bff0e0d2
Пользователь <lilu163@mail.ru>
2024-08-03 23:05:26 +0400

    start

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ function getCurrentWeather() {
 
 function setZoom() {
   if (window.matchMedia('(min-width: 360px) and (max-width: 1280px)').matches) {
-  document.body.style.zoom = "32%";
+  document.body.style.zoom = "24%";
   } else {
   document.body.style.zoom = "100%";
   }
